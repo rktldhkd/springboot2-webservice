e88f4225e8f60306684e9eb8501c531f03bef334
RCH <rktldhkd@naver.com>
2020-08-25 12:15:19 +0900

    카카오소셜로그인 기능추가.

--- src/main/java/com/jojoldu/book/springboot/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/jojoldu/book/springboot/config/auth/dto/OAuthAttributes.java
@@ -32,6 +32,7 @@ public class OAuthAttributes {
         //userNameAttributeName : OAuth2 로그인 진행시 키가 되는 필드값.(pk) 구글만 기본값제공(sub). 나머지는 기본값없음.
 
         if("naver".equals(registrationId))  return ofNaver("id", attributes);
+        if("kakao".equals(registrationId))  return ofKakao("id", attributes);
 
         return ofGoogle(userNameAttributeName, attributes);
     }
@@ -59,6 +60,29 @@ public class OAuthAttributes {
                     .attributes(response)
                     .nameAttributeKey(userNameAttributeName)
                     .build();
+    }
+
+    //카카오 로그인
+    private static OAuthAttributes ofKakao(String userNameAttributeName, Map<String,Object> attributes){
+//        for (Map.Entry<String, Object> entry : attributes.entrySet()) {
+//            System.out.println("[Key]:" + entry.getKey() + " [Value]:" + entry.getValue());
+//        }
+
+        //kakao로그인 성공시 가져오는 JSON데이터들 중, kakao_account 항목의 값. kakao_account항목은 사용자 정보를 가지고 있다.
+        Map<String,Object> kakao_account = (Map<String,Object>)attributes.get("kakao_account");
+        Map<String,Object> profile = (Map<String,Object>)kakao_account.get("profile");
+
+        //카카오는 최상위에 id, kakao_account json값 항목이 따로있다. id값까지 보내려면 최상위객체에서 뽑아와야함.
+        kakao_account.put("id", attributes.get("id"));
+
+        //사진은 없어서 picture에 그냥 nickname이나 넣어줌.
+        return OAuthAttributes.builder()
+                .name((String)profile.get("nickname"))
+                .email((String)kakao_account.get("email"))
+                .picture((String)profile.get("nickname"))
+                .attributes(attributes)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
     }
 
     //User엔티티 생성
